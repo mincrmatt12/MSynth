9969e1b3a8900aa4725f83216015a9a1cd7f6355
mincrmatt12 <matthew@mm12.xyz>
2020-01-25 17:18:27 -0500

    framework: lib: lcd style fix

--- framework/lib/src/lcd.cpp
+++ framework/lib/src/lcd.cpp
@@ -130,7 +130,7 @@ void lcd::init() {
 	LTDC->SSCR = (40 << LTDC_SSCR_HSW_Pos) | (9 << LTDC_SSCR_VSH_Pos); // 1 - v/hsync
 	LTDC->BPCR = ((41 + 2 - 1) << LTDC_BPCR_AHBP_Pos) | ((10 + VFP - 1) << LTDC_BPCR_AVBP_Pos); // sync + back porch - 1
 	LTDC->AWCR = ((41 + 2 + 480 - 1) << LTDC_AWCR_AAW_Pos) | ((10 + VFP + 272 - 1) << LTDC_AWCR_AAH_Pos);
-	LTDC->TWCR = (524 << LTDC_TWCR_TOTALW_Pos) | (10 + VFP + 272 + 2 - 1 << LTDC_TWCR_TOTALH_Pos);
+	LTDC->TWCR = (524 << LTDC_TWCR_TOTALW_Pos) | ((10 + VFP + 272 + 2 - 1) << LTDC_TWCR_TOTALH_Pos);
 
 	// Setup clock polarity
 	LTDC->GCR &= ~(LTDC_GCR_PCPOL | LTDC_GCR_HSPOL | LTDC_GCR_VSPOL | LTDC_GCR_DEPOL);
