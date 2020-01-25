6e29b906df6b618e7379c83eeb5ef93860dee24e
mincrmatt12 <matthew@mm12.xyz>
2020-01-25 17:18:04 -0500

    bootloader: retrieve app return code

--- bootloader/src/boot.c
+++ bootloader/src/boot.c
@@ -41,17 +41,19 @@ void boot_app(const app_hdr *app) {
 
 void __attribute__((naked)) app_return_handler() {
 	asm volatile (
+		"mov r2, r0\n\t"           // Save the return value
 		"ldr r0, =0x08000000\n\t"  // Load our initial stack value (almost certainly the top of CCMRAM)
 		"ldr r0, [r0]\n\t"         // *(0x0800 0000)
 		"msr msp, r0\n\t"          // set that stack
 		"ldr r1, =app_return_handler_impl\n\t"  // app_return_handler_impl(), now with a sane stack
+		"mov r0, r2\n\t"           // Pass the original return to this function
 		"bx r1\n\t"
 	);
 }
 
 extern int _write(int, const char *, int);
 
-void __attribute__((noreturn)) app_return_handler_impl() {
+void __attribute__((noreturn)) app_return_handler_impl(uint32_t return_from_func) {
 	// rerun sys-init
 	sys_init();
 	// directly _write (since we don't re-init libc)
