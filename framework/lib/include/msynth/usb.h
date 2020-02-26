c43c73fac2f1f52df96e900d0524a915a42cb829
mincrmatt12 <matthew@mm12.xyz>
2020-02-26 12:00:15 -0500

    framework: usb: split into HostBase and Host

--- framework/lib/include/msynth/usb.h
+++ framework/lib/include/msynth/usb.h
@@ -116,7 +116,8 @@ namespace usb {
 		typename std::aligned_union<0, SupportedDevices...>::type storage;
 	};
 
-	
+	// DynamicStateHolder - allocates its objects on the heap and calls the correct destructor. Because the types still don't inherit from
+	// a common base, they don't have a virtual destructor, so we still need some cast logic.
 	template<typename ...SupportedDevices>
 	struct DynamicStateHolder {
 		template<typename T>
@@ -159,8 +160,56 @@ namespace usb {
 		void * held = nullptr;
 	};
 
+	// HostBase is a low-level type that implements most of the logic in Host. It is effectively the template-invariant
+	// components of Host, such as low-level OTG_HS management and IRQ handling.
+	//
+	// An application is free to use this type if it desires, although automated enumeration and other nice things are not
+	// handled by it.
+	struct HostBase {
+		// Non-copyable type
+		HostBase(const HostBase& other) = delete;
+		// Non-moveable type (global/references only)
+		HostBase(HostBase &&other) = delete;
+		// Default constructor
+		HostBase() {};
+
+		// POWER MANAGEMENT
+		// (usb-level)
+
+		// Enable port power. This method requires that init() has been called.
+		//
+		// Once enabled, a peripheral may be detected at any time. It is wise (although not strictly required) to poll inserted() after
+		// calling this function, so that devices which check if a host is smart don't get confused.
+		void enable();
+		void disable();
+
+		// PCDET
+		inline bool inserted() {
+			return USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PCSTS;
+		}
+
+		// INITIALIZATION
+
+		// Setup the host after a PCDET interrupt.
+		init_status init_host_after_connect();
+
+		// Initialize the host AHB-peripheral. This function will only work correctly if interrupts are set up correctly.
+		// At the very least, ensure the USB global interrupt goes to the correct irq function. If overcurrent is not mapped correctly
+		// the system may hang if the connected peripheral triggers an overcurrent situation.
+		void init();
+
+		// INTERRUPTS
+		void overcurrent_irq();
+		void usb_global_irq(); 
+	private:
+		// Various state flags:
+		// These are kept in an app-controlled variable because the peripheral docs are unclear.
+		// Some of these flags in the USB are cleared as part of the interrupt scheme.
+		volatile uint16_t got_penchng : 1;
+	};
+
 	template<template<typename...> typename StateHolderImpl, typename ...SupportedDevices>
-	struct Host {
+	struct Host : private HostBase {
 		using StateHolder = StateHolderImpl<SupportedDevices...>;
 
 		// Non-copyable type
@@ -176,196 +225,33 @@ namespace usb {
 		// POWER MANAGEMENT
 		//
 		// Detection of inserted devices is elsewhere
-		
-		// Enable port power. This method requires that init() has been called.
-		//
-		// Once enabled, a peripheral may be detected at any time. It is wise (although not strictly required) to poll inserted() after
-		// calling this function, so that devices which check if a host is smart don't get confused.
-		void enable() {
-			// Enable the HPRT interrupts
-			USB_OTG_HS->GINTMSK |= USB_OTG_GINTMSK_PRTIM;
-			// Set FS
-			USB_OTG_HS_HOST->HCFG = 0b101; // Set 48mhz FS (will adjust)
 
-			// Turn on port power
-			LL_GPIO_SetOutputPin(GPIOE, LL_GPIO_PIN_2); // dwrrrrrrrrrn
-			USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PPWR;
+		using HostBase::enable;
 
-			// Debounce
-			util::delay(200);
-		}
 		// Kill port power. This hard disconnects the device, and init_periph will need to be called again.
 		//
 		// Note that it is not necessary to handle overcurrents manually, as this class takes care of this functionality internally.
 		void disable() {
-			// Check the state of the USB
-			if (inserted()) {
-				// Device was inserted, so let's disable the port
-				USB_OTG_HS_HPRT0 &= ~(USB_OTG_HPRT_PENA);
-
-				util::delay(5);
-
-				device.reset();
-			}
-
-			// Kill port power
-			USB_OTG_HS_HPRT0 &= ~(USB_OTG_HPRT_PPWR);
-			LL_GPIO_ResetOutputPin(GPIOE, LL_GPIO_PIN_2);
-
-			// TODO: Clean up after the old thing
+			HostBase::disable();
+			device.reset();
 		}
 
 		// PERIPHERAL DETECTION
-		bool inserted() {
-			return USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PCSTS;
-		}
+		using HostBase::inserted;
 
 		// INITIALIZATION
 		
 		// Initialize a connected peripheral. To check which peripheral was connected (if init_status == Ok)
 		// use the template specializations of inserted.
 		init_status init_periph() {
-			if (!inserted()) return init_status::NotInserted;
+			// Initialize the USB low-level stuff for enumeration
+			if (auto result = HostBase::init_host_after_connect(); result != init_status::Ok) return result;
 
-change_speed:
-			// Start by resetting the port
-			USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PRST;
-			// Wait at least twice the value in the datasheet, you never know what bullcrap is in them these days
-			util::delay(15);
-			USB_OTG_HS_HPRT0 &= ~(USB_OTG_HPRT_PRST);
-			// Wait for a PENCHNG interrupt
-			while (!got_penchng) {;}
-			got_penchng = 0;
-
-			// Check enumerated speed
-			if ((USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PSPD) == USB_OTG_HPRT_PSPD_0) {
-				// Full speed
-				if ((USB_OTG_HS_HOST->HCFG & USB_OTG_HCFG_FSLSPCS) != 1) {
-					USB_OTG_HS_HOST->HCFG = 0b101; // Set the speed 
-					util::delay(1);
-					// Reset again
-					goto change_speed;
-				}
-
-				USB_OTG_HS_HOST->HFIR = 47999U;
-			}
-			else if ((USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PSPD) == USB_OTG_HPRT_PSPD_1) {
-				// Low speed
-				if ((USB_OTG_HS_HOST->HCFG & USB_OTG_HCFG_FSLSPCS) != 2) {
-					USB_OTG_HS_HOST->HCFG = 0b110; // Set the speed 
-					util::delay(1);
-					// Reset again
-					goto change_speed;
-				}
-
-				USB_OTG_HS_HOST->HFIR = 6999U;
-			}
-			else {
-				// not supported
-				return init_status::NotSupported;
-			}
-
-			// Set the FIFO sizes
-			//
-			// Addresses are in units of words, maximum 0x140
-			// We set up the bus with:
-			// 	- 0x80 (128 word) RX and non-periodic TX fifo
-			// 	- 0x3C (60 word) TX periodic fifo
-			
-			USB_OTG_HS->GRXFSIZ = 0x100;
-			USB_OTG_HS->DIEPTXF0_HNPTXFSIZ = (0x80 << 16) | 0x100 /* start address */;
-			USB_OTG_HS->HPTXFSIZ = (0x40 << 16) | 0x180;
-
-			// We are now inited at the host level. Begin device enumeration.
+			// Begin enumeration
 			return init_status::NotSupported;
 		}
 
-		// Initialize the host AHB-peripheral. This function will only work correctly if interrupts are set up correctly.
-		// At the very least, ensure the USB global interrupt goes to the correct irq function. If overcurrent is not mapped correctly
-		// the system may hang if the connected peripheral triggers an overcurrent situation.
-		void init() {
-			LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_OTGHS);
-			LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-			LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOE);
-
-			// Try resetting it the AHB way
-			LL_AHB1_GRP1_ForceReset(LL_AHB1_GRP1_PERIPH_OTGHS);
-
-			util::delay(2);
-
-			LL_AHB1_GRP1_ReleaseReset(LL_AHB1_GRP1_PERIPH_OTGHS);
-
-			// Setup the power switch GPIOs
-			{
-				LL_GPIO_InitTypeDef init;
-				init.Mode = LL_GPIO_MODE_OUTPUT;
-				init.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-				init.Pull = LL_GPIO_PULL_NO;
-				init.Pin = LL_GPIO_PIN_2;
-				init.Speed = LL_GPIO_SPEED_FREQ_LOW;
-
-				LL_GPIO_Init(GPIOE, &init); // USB_EN
-
-				init.Pin = LL_GPIO_PIN_3;
-				init.Mode = LL_GPIO_MODE_INPUT;
-				init.Pull = LL_GPIO_PULL_UP;
-
-				LL_GPIO_Init(GPIOE, &init); // USB_FAULT
-
-				init.Pin = LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
-				init.Mode = LL_GPIO_MODE_ALTERNATE;
-				init.Alternate = LL_GPIO_AF_12; // this isn't in the datasheet but it's in the examples
-				init.Pull = LL_GPIO_PULL_NO;
-				init.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-
-				LL_GPIO_Init(GPIOB, &init);
-			}
-
-			while (USB_OTG_HS->GRSTCTL & USB_OTG_GRSTCTL_CSRST) {;}
-			
-			// Initialize USB
-			USB_OTG_HS->GUSBCFG |= USB_OTG_GUSBCFG_FHMOD;
-			USB_OTG_HS->GUSBCFG &= ~USB_OTG_GUSBCFG_TRDT;
-			USB_OTG_HS->GUSBCFG |= (0x9 << USB_OTG_GUSBCFG_TRDT_Pos);
-			USB_OTG_HS->GUSBCFG &= ~(USB_OTG_GUSBCFG_HNPCAP | USB_OTG_GUSBCFG_SRPCAP);
-			USB_OTG_HS->GUSBCFG |= USB_OTG_GUSBCFG_PHYLPCS | USB_OTG_GUSBCFG_PHYSEL;
-
-			// Disable DMAmode
-			USB_OTG_HS->GAHBCFG &= ~(USB_OTG_GAHBCFG_DMAEN);
-
-			// Clear all interrupts
-			USB_OTG_HS->GINTSTS = 0xFFFFFFFF; // technically not compliant but screw it
-
-			// Power UP PHY
-			USB_OTG_HS_PCGCCTL = 0;
-			USB_OTG_HS->GCCFG |= USB_OTG_GCCFG_PWRDWN;
-
-			// Disable sensing
-			USB_OTG_HS->GCCFG &= ~(USB_OTG_GCCFG_VBUSASEN | USB_OTG_GCCFG_VBUSBSEN);
-			USB_OTG_HS->GCCFG |= USB_OTG_GCCFG_NOVBUSSENS;
-			// Disable SOF
-			USB_OTG_HS->GCCFG &= ~USB_OTG_GCCFG_SOFOUTEN;
-		
-			// Flush all FIFOs
-			USB_OTG_HS->GRSTCTL = (15 << USB_OTG_GRSTCTL_TXFNUM_Pos) | USB_OTG_GRSTCTL_TXFFLSH;
-			while (USB_OTG_HS->GRSTCTL & USB_OTG_GRSTCTL_TXFFLSH) {;}
-			USB_OTG_HS->GRSTCTL |= USB_OTG_GRSTCTL_RXFFLSH;
-			while (USB_OTG_HS->GRSTCTL & USB_OTG_GRSTCTL_RXFFLSH) {;}
-
-			// Enable mode mismatch/OTG interrupts
-			USB_OTG_HS->GINTMSK = 0;
-			USB_OTG_HS->GINTMSK |= USB_OTG_GINTMSK_MMISM | USB_OTG_GINTMSK_OTGINT;
-
-			// Enable USB IRQ
-			NVIC_SetPriority(OTG_HS_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 2, 0));
-			NVIC_EnableIRQ(OTG_HS_IRQn);
-
-			// Enable interrupts
-			USB_OTG_HS->GAHBCFG |= USB_OTG_GAHBCFG_GINT;
-
-			// We are now inited. To continue host initilaization,
-			// the user must call enable();
-		}
+		using HostBase::init;
 
 		// DEVICE STATE / API
 		template<typename T>
@@ -389,28 +275,9 @@ change_speed:
 		}
 
 		// INTERRUPTS
-		void overcurrent_irq();
-		void usb_global_irq() {
-			// Check what triggered the interrupt
-			if (USB_OTG_HS->GINTSTS & USB_OTG_GINTSTS_HPRTINT) {
-				// Host port interrupt
-				if (USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PENCHNG) {
-					// Port enable status changed
-					got_penchng = 1;
-					USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PENCHNG;
-				}
-				else if (USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PCDET) {
-					// Port connection detected, for now just a no-op.
-					USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PCDET; // clear
-				}
-			}
-		}
+		using HostBase::overcurrent_irq;
+		using HostBase::usb_global_irq;
 	private:
-		// Various state flags:
-		// These are kept in an app-controlled variable because the peripheral docs are unclear.
-		// Some of these flags in the USB are cleared as part of the interrupt scheme.
-		volatile uint16_t got_penchng : 1;
-
 		StateHolder device;
 	};
 
--- framework/lib/src/usb.cpp
+++ framework/lib/src/usb.cpp
@@ -1,3 +1,197 @@
 #include <usb.h>
 
 // This file contains various device types.
+// as well as HostBase
+
+void usb::HostBase::enable() {
+	// Enable the HPRT interrupts
+	USB_OTG_HS->GINTMSK |= USB_OTG_GINTMSK_PRTIM;
+	// Set FS
+	USB_OTG_HS_HOST->HCFG = 0b101; // Set 48mhz FS (will adjust)
+
+	// Turn on port power
+	LL_GPIO_SetOutputPin(GPIOE, LL_GPIO_PIN_2); // dwrrrrrrrrrn
+	USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PPWR;
+
+	// Debounce
+	util::delay(200);
+}
+
+void usb::HostBase::disable() {
+	// Check the state of the USB
+	if (inserted()) {
+		// Device was inserted, so let's disable the port
+		USB_OTG_HS_HPRT0 &= ~(USB_OTG_HPRT_PENA);
+
+		util::delay(5);
+
+	}
+
+	// Kill port power
+	USB_OTG_HS_HPRT0 &= ~(USB_OTG_HPRT_PPWR);
+	LL_GPIO_ResetOutputPin(GPIOE, LL_GPIO_PIN_2);
+
+	// TODO: Clean up after the old thing
+	// End all channels
+	// Clear all FIFOs
+	// Cause nuclear reset
+}
+
+usb::init_status usb::HostBase::init_host_after_connect() {
+	if (!inserted()) return init_status::NotInserted;
+
+change_speed:
+	// Start by resetting the port
+	USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PRST;
+	// Wait at least twice the value in the datasheet, you never know what bullcrap is in them these days
+	util::delay(15);
+	USB_OTG_HS_HPRT0 &= ~(USB_OTG_HPRT_PRST);
+	// Wait for a PENCHNG interrupt
+	while (!got_penchng) {;}
+	got_penchng = 0;
+
+	// Check enumerated speed
+	if ((USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PSPD) == USB_OTG_HPRT_PSPD_0) {
+		// Full speed
+		if ((USB_OTG_HS_HOST->HCFG & USB_OTG_HCFG_FSLSPCS) != 1) {
+			USB_OTG_HS_HOST->HCFG = 0b101; // Set the speed 
+			util::delay(1);
+			// Reset again
+			goto change_speed;
+		}
+
+		USB_OTG_HS_HOST->HFIR = 47999U;
+	}
+	else if ((USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PSPD) == USB_OTG_HPRT_PSPD_1) {
+		// Low speed
+		if ((USB_OTG_HS_HOST->HCFG & USB_OTG_HCFG_FSLSPCS) != 2) {
+			USB_OTG_HS_HOST->HCFG = 0b110; // Set the speed 
+			util::delay(1);
+			// Reset again
+			goto change_speed;
+		}
+
+		USB_OTG_HS_HOST->HFIR = 6999U;
+	}
+	else {
+		// not supported
+		return init_status::NotSupported;
+	}
+
+	// Set the FIFO sizes
+	//
+	// Addresses are in units of words, maximum 0x140
+	// We set up the bus with:
+	// 	- 0x80 (128 word) RX and non-periodic TX fifo
+	// 	- 0x3C (60 word) TX periodic fifo
+
+	USB_OTG_HS->GRXFSIZ = 0x100;
+	USB_OTG_HS->DIEPTXF0_HNPTXFSIZ = (0x80 << 16) | 0x100 /* start address */;
+	USB_OTG_HS->HPTXFSIZ = (0x40 << 16) | 0x180;
+
+	// We are now inited at the host level. Begin device enumeration.
+
+	// At this point, the device has completed reset and is responding on ADDR 0
+	return init_status::Ok;
+}
+
+void usb::HostBase::init() {
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_OTGHS);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOE);
+
+	// Try resetting it the AHB way
+	LL_AHB1_GRP1_ForceReset(LL_AHB1_GRP1_PERIPH_OTGHS);
+
+	util::delay(2);
+
+	LL_AHB1_GRP1_ReleaseReset(LL_AHB1_GRP1_PERIPH_OTGHS);
+
+	// Setup the power switch GPIOs
+	{
+		LL_GPIO_InitTypeDef init;
+		init.Mode = LL_GPIO_MODE_OUTPUT;
+		init.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+		init.Pull = LL_GPIO_PULL_NO;
+		init.Pin = LL_GPIO_PIN_2;
+		init.Speed = LL_GPIO_SPEED_FREQ_LOW;
+
+		LL_GPIO_Init(GPIOE, &init); // USB_EN
+
+		init.Pin = LL_GPIO_PIN_3;
+		init.Mode = LL_GPIO_MODE_INPUT;
+		init.Pull = LL_GPIO_PULL_UP;
+
+		LL_GPIO_Init(GPIOE, &init); // USB_FAULT
+
+		init.Pin = LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
+		init.Mode = LL_GPIO_MODE_ALTERNATE;
+		init.Alternate = LL_GPIO_AF_12; // this isn't in the datasheet but it's in the examples
+		init.Pull = LL_GPIO_PULL_NO;
+		init.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+
+		LL_GPIO_Init(GPIOB, &init);
+	}
+
+	while (USB_OTG_HS->GRSTCTL & USB_OTG_GRSTCTL_CSRST) {;}
+
+	// Initialize USB
+	USB_OTG_HS->GUSBCFG |= USB_OTG_GUSBCFG_FHMOD;
+	USB_OTG_HS->GUSBCFG &= ~USB_OTG_GUSBCFG_TRDT;
+	USB_OTG_HS->GUSBCFG |= (0x9 << USB_OTG_GUSBCFG_TRDT_Pos);
+	USB_OTG_HS->GUSBCFG &= ~(USB_OTG_GUSBCFG_HNPCAP | USB_OTG_GUSBCFG_SRPCAP);
+	USB_OTG_HS->GUSBCFG |= USB_OTG_GUSBCFG_PHYLPCS | USB_OTG_GUSBCFG_PHYSEL;
+
+	// Disable DMAmode
+	USB_OTG_HS->GAHBCFG &= ~(USB_OTG_GAHBCFG_DMAEN);
+
+	// Clear all interrupts
+	USB_OTG_HS->GINTSTS = 0xFFFFFFFF; // technically not compliant but screw it
+
+	// Power UP PHY
+	USB_OTG_HS_PCGCCTL = 0;
+	USB_OTG_HS->GCCFG |= USB_OTG_GCCFG_PWRDWN;
+
+	// Disable sensing
+	USB_OTG_HS->GCCFG &= ~(USB_OTG_GCCFG_VBUSASEN | USB_OTG_GCCFG_VBUSBSEN);
+	USB_OTG_HS->GCCFG |= USB_OTG_GCCFG_NOVBUSSENS;
+	// Disable SOF
+	USB_OTG_HS->GCCFG &= ~USB_OTG_GCCFG_SOFOUTEN;
+
+	// Flush all FIFOs
+	USB_OTG_HS->GRSTCTL = (15 << USB_OTG_GRSTCTL_TXFNUM_Pos) | USB_OTG_GRSTCTL_TXFFLSH;
+	while (USB_OTG_HS->GRSTCTL & USB_OTG_GRSTCTL_TXFFLSH) {;}
+	USB_OTG_HS->GRSTCTL |= USB_OTG_GRSTCTL_RXFFLSH;
+	while (USB_OTG_HS->GRSTCTL & USB_OTG_GRSTCTL_RXFFLSH) {;}
+
+	// Enable mode mismatch/OTG interrupts
+	USB_OTG_HS->GINTMSK = 0;
+	USB_OTG_HS->GINTMSK |= USB_OTG_GINTMSK_MMISM | USB_OTG_GINTMSK_OTGINT;
+
+	// Enable USB IRQ
+	NVIC_SetPriority(OTG_HS_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 2, 0));
+	NVIC_EnableIRQ(OTG_HS_IRQn);
+
+	// Enable interrupts
+	USB_OTG_HS->GAHBCFG |= USB_OTG_GAHBCFG_GINT;
+
+	// We are now inited. To continue host initilaization,
+	// the user must call enable();
+}
+
+void usb::HostBase::usb_global_irq() {
+	// Check what triggered the interrupt
+	if (USB_OTG_HS->GINTSTS & USB_OTG_GINTSTS_HPRTINT) {
+		// Host port interrupt
+		if (USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PENCHNG) {
+			// Port enable status changed
+			got_penchng = 1;
+			USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PENCHNG;
+		}
+		else if (USB_OTG_HS_HPRT0 & USB_OTG_HPRT_PCDET) {
+			// Port connection detected, for now just a no-op.
+			USB_OTG_HS_HPRT0 |= USB_OTG_HPRT_PCDET; // clear
+		}
+	}
+}
+
