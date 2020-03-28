5cb8869d7624f37718ab38030b64433f314df3b9
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-03-28 17:33:48 -0400

    bootloader: hold F4 to force TEST

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -2,15 +2,18 @@
 #include "sys.h"
 #include "applist.h"
 #include "boot.h"
+#include "ui.h"
 
 int main() {
 	// Start the bootloader
 	sys_init();
 	// lcd_init();
-	// ui_init();
+	ui_init();
 	// eeprom_init();
 	
-	puts("MSignBoot v0");
+	puts("MSignBoot v1");
+
+	uint32_t held_code = ui_get_button();
 
 	// Find all the apps
 	detect_app_and_bucket_headers();
@@ -18,8 +21,8 @@ int main() {
 	if (appheader_count == 0) {
 		puts("No apps are installed");
 	}
-	else {
-		// TEMP: boot first primary app.
+	else if (!held_code) {
+		// No special code, boot first priamry app
 		
 		for (int i = 0; i < appheader_count; ++i) {
 			printf("got app %s; id %08lx\n", app_headers_found[i]->name, app_headers_found[i]->appid);
@@ -32,6 +35,16 @@ int main() {
 		puts("no primary app");
 		// We haven't booted an app, try the TEST app
 		//
+		for (int i = 0; i < appheader_count; ++i) {
+			if (app_headers_found[i]->flags & APPHEAD_FLAG_ISTEST) {
+				puts("booting test");
+				boot_app(app_headers_found[i]);
+			}
+		}
+	}
+	else if (held_code & UI_BUTTON_F4) {
+		puts("override boot mode; test");
+		// Try and boot the test app.
 		for (int i = 0; i < appheader_count; ++i) {
 			if (app_headers_found[i]->flags & APPHEAD_FLAG_ISTEST) {
 				puts("booting test");
