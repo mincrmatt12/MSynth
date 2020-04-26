571098ad90dc67ad03caf814ee467231c5462476
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-04-25 22:00:52 -0400

    app: event: overcomplicated attempt

---
+++ app/main/src/ui/evt/dispatch.h
@@ -0,0 +1,62 @@
+#pragma once
+
+// The core part of the event system, containing all the base definitions for events.
+// To save some RAM, all UI screens are placed onto the heap. They form a stack (for dialogs), and only
+// the topmost one sticks around.
+//
+// Other stuff (like buffers) go onto RAM, and song data is stored wherever (TODO)
+//
+// Because the event listeners are constructed and deconstructed at semantically relevant times, however, we can use some
+// fairly simple logic to handle event processing, based on constructors.
+//
+// Events are source-agnostic representations of interactions. For example:
+// 	- TouchEvent: touchscreen/mouse (abusing LTDC layer 2 mouse cursor is handled by the input code)
+// 		- pressure, x, y
+// 	- KeyEvent: button press/hid keyboard
+// 		- state (down, up, press), key-id (translated if applicable; key combinations are handled separately)
+// 	- etc. (various state events so the UI/system can be informed of stuff)
+//
+// UI's must handle some subset of them by implementing EventHandler<SomeEvents>
+//
+// This exposes some internal stuff which allows the resulting type to be used by the event handling system.
+
+#include <stdint.h>
+#include <type_traits>
+
+namespace ms::evt {
+	// All events must have a const static public member named "id" which is a unique integer.
+	//
+	// These form a bitmask of up to 32 entries.
+	
+	template<typename Event>
+	constexpr static inline bool is_event_v = std::is_integral_v<decltype(Event::id)> && Event::id < 32;
+
+	// Dispatch an event.
+	template<typename Event>
+	inline std::enable_if_t<is_event_v<Event>> dispatch(const Event& evt) {
+		dispatch(&evt, Event::id);
+	}
+
+	// Helper declaration to convert parameter pack to a bitmask of events
+	template<typename ...Events>
+	constexpr static inline std::enable_if_t<std::conjunction_v<is_event_v<Events>...>, uint32_t> events_to_bitmask = (Events::id | ...);
+
+	// Internal helper to encapsulate the correct inheritance
+	template<typename Event>
+	struct callback_holder {
+		virtual void handle(const Event& evt) = 0;
+	};
+
+	// Base event handler type
+	template<typename ...ListeningFor>
+	struct EventHandler : public callback_holder<ListeningFor>... {
+		// TODO: add the correct friend definitions so the private dispatch is available
+
+		static const inline uint32_t bitmask = events_to_bitmask<ListeningFor...>;
+	private:
+		void dispatch(const void *opaque, int id) {
+			if (!((1 << id) & bitmask)) return;
+			((id == ListeningFor::id && (handle(*reinterpret_cast<const ListeningFor *>(opaque)), true)) || ...);
+		}
+	};
+}
