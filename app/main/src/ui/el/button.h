542b33dbafa0c5d6d9e1a40adef1fc8413e54e0e
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-04-30 16:13:18 -0400

    app: ui: separate button into cpp

--- app/main/src/ui/el/button.h
+++ app/main/src/ui/el/button.h
@@ -22,29 +22,8 @@ namespace ms::ui::element {
 		using LayoutTraits = l::LayoutTraits<BoxLayoutParams::InsideTrait, l::traits::MouseTypes<evt::TouchEvent::StatePressed> /* todo some enabled check (IfFlag<FlagEnabled>) */, 
 			  l::traits::ForceIf<l::traits::MouseTypes<evt::TouchEvent::StateReleased>>>;
 
-		bool handle(const evt::TouchEvent& evt, const LayoutParams& lp) {
-			if (evt.state == evt::TouchEvent::StateReleased && flag(FlagPressed)) {
-				toggle_flag(FlagPressed);
-				mark_dirty();
-				cb();
-			}
-			else if (!flag(FlagPressed)) {
-				toggle_flag(FlagPressed);
-				mark_dirty();
-				return true;
-			}
-			return false;
-		}
-
-		void draw(const LayoutParams& lp) {
-			draw::outline(lp.bbox.x, lp.bbox.y, lp.bbox.x + lp.bbox.w, lp.bbox.y + lp.bbox.h, flag(FlagPressed) ? 0b11'01'11'01 : 0b11'10'10'10);
-			draw::rect(lp.bbox.x + 1, lp.bbox.y + 1, lp.bbox.x + lp.bbox.w, lp.bbox.y + lp.bbox.h, flag(FlagPressed) ? 0b11'00'01'00 : 0b11'01'01'01);
-
-			// center text
-			int16_t cx = lp.bbox.x + lp.bbox.w / 2 - (draw::text_size(label, ui_16_font) / 2);
-			int16_t cy = lp.bbox.y + lp.bbox.h / 2 + 5; // ascender height from top (13) - center of font (16/2 = 8)
-			draw::text(cx, cy, label, ui_16_font, 0xff);
-		}
+		bool handle(const evt::TouchEvent& evt, const LayoutParams& lp); 
+		void draw(const LayoutParams& lp); 
 
 	private:
 		const char *label;
