055ebafd74d2d74e109b560497432aea8de6fb8c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-04-30 21:56:01 -0400

    app: ui: super basic text element

---
+++ app/main/src/ui/el/text.cpp
@@ -0,0 +1,25 @@
+#include "text.h"
+
+void ms::ui::element::Text::draw(const LayoutParams& lp) {
+	// TODO: get the font and baseline
+	
+	const void * font = ui_16_font;
+
+	// Honor alignment rules
+	uint16_t width = draw::text_size(label, font);
+	
+	int16_t x = align_in(width, lp.bbox.w, lp.horiz_align);
+	int16_t y = align_in(16, lp.bbox.h, lp.vert_align) + 5; // TODO: use baseline
+
+	draw::text(lp.bbox.x + x, lp.bbox.y + y, label, font, color);
+}
+
+void ms::ui::element::Text::set_label(const char * text) {
+	this->label = text;
+	mark_dirty();
+}
+
+void ms::ui::element::Text::set_color(uint8_t color) {
+	this->color = color;
+	mark_dirty();
+}
---
+++ app/main/src/ui/el/text.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../base.h"
+#include <msynth/util.h>
+#include <msynth/draw.h>
+
+namespace ms::ui::element {
+	// A fairly basic text rendering component.
+	//
+	// Allows you to set the text color and content at runtime, and alignment options are provided through the parameters.
+	struct Text : ElementFlagsProvider<1> {
+		Text(const char * label, uint8_t color) : color(color), label(label) {
+			set_flag(FlagDirty, true);
+		}
+
+		struct LayoutParams : BoxLayoutParams {
+			Align horiz_align = AlignCenter, vert_align = AlignCenter;
+
+			// TODO: font selection
+
+			constexpr LayoutParams(const Box& box, Align horiz=AlignCenter, Align vert=AlignCenter) : BoxLayoutParams(box), horiz_align(horiz), vert_align(vert) {}
+			constexpr LayoutParams(const Box& box, LayoutParams&& lp) : LayoutParams(box, lp.horiz_align, lp.vert_align) {}
+		};
+
+		using LayoutTraits = l::LayoutTraits<BoxLayoutParams::NonInteractableTraitRequirement>;
+
+		void draw(const LayoutParams& lp); 
+		void set_label(const char * text);
+		void set_color(uint8_t color);
+
+	private:
+		uint8_t color;
+		const char *label;
+	};
+}
