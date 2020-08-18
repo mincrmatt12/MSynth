93b24276a243ddc45f36b14dfaf3ed3637a7f88e
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-08-17 21:43:57 -0400

    app: irq: hookup audio dma interrupt

--- app/main/src/irq.cpp
+++ app/main/src/irq.cpp
@@ -5,6 +5,8 @@
 #include <msynth/util.h>
 #include <msynth/sd.h>
 
+#include "audio.h"
+
 // ISRs
 
 ISR(SDIO) {
@@ -16,6 +18,12 @@ ISR(USB_OTG_HS) {
 	ms::in::usb_host.usb_global_irq();
 }
 
+ISR(DMA1_Stream4) {
+	ms::audio::dma_interrupt();
+}
+
 void ms::irq::init() {
-	// Currently no interesting IRQs.
+	// Enable DMA interrupts.
+	NVIC_EnableIRQ(DMA1_Stream4_IRQn);
+	NVIC_SetPriority(DMA1_Stream4_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 5, 0)); // TODO: pick a reasonable priority for this.
 }
