5c573e32df0dcde4a379027f0c9b37b2aab969fd
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-08-21 01:10:42 -0400

    app: synth: add semitone to multfactor

--- app/main/src/synth/util.h
+++ app/main/src/synth/util.h
@@ -9,6 +9,10 @@ namespace ms::synth {
 		return powf(2.f, static_cast<float>(midi_note - 69.f) / 12.f) * 440.f;
 	}
 
+	inline float offset_scale_from_semitones(float semitones) {
+		return powf(2.f, (semitones / 24.f));
+	}
+
 	// sin, but it's scaled to 0-1 as a repeating wave.
 	//
 	// and also uses a lookup table for high speed
