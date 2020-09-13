590622bc9fd6cbcd1ebf8549b8ee5d82e89fda1a
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-09-13 19:38:08 -0400

    app: modules(env): fix adsr sustain + exp curves

--- app/main/src/synth/modules/env.cpp
+++ app/main/src/synth/modules/env.cpp
@@ -4,17 +4,17 @@
 bool ms::synth::mod::ADSR::generate(const Cfg& cfg) {
 	output = 0.f;
 
-	if (off_time < 0.f && curr_time < cfg.attack_time) {
+	if (curr_time < cfg.attack_time) {
 		output = (curr_time / cfg.attack_time) * cfg.attack_level;
 	}
-	else if (off_time < 0.f && curr_time < (cfg.attack_time + cfg.decay_time)) {
+	else if (curr_time < (cfg.attack_time + cfg.decay_time)) {
 		output = cfg.attack_level - ((curr_time - cfg.attack_time) / cfg.decay_time) * (cfg.attack_level - cfg.sustain_level);
 	}
-	else if (off_time < 0.f) {
+	else {
 		output = cfg.sustain_level;
 	}
-	else {
-		output = cfg.sustain_level - ((curr_time - off_time) / cfg.release_time) * cfg.sustain_level;
+	if (off_time >= 0.f) {
+		output -= ((curr_time - off_time) / cfg.release_time) * output;
 	}
 
 	output *= scale;
@@ -26,17 +26,17 @@ bool ms::synth::mod::ADSR::generate(const Cfg& cfg) {
 bool ms::synth::mod::ExpADSR::generate(const Cfg& cfg) {
 	output = 0.f;
 
-	if (off_time < 0.f && curr_time < cfg.attack_time) {
+	if (curr_time < cfg.attack_time) {
 		output = (1.f - powf(1.f - curr_time / cfg.attack_time, 2.f)) * cfg.attack_level;
 	}
-	else if (off_time < 0.f && curr_time < (cfg.attack_time + cfg.decay_time)) {
-		output = cfg.attack_level - powf((curr_time - cfg.attack_time) / cfg.decay_time, 2.f) * (cfg.attack_level - cfg.sustain_level);
-	}
-	else if (off_time < 0.f) {
-		output = cfg.sustain_level;
+	else if (curr_time < (cfg.attack_time + cfg.decay_time)) {
+		output = cfg.sustain_level + powf(1.f - (curr_time - cfg.attack_time) / cfg.decay_time, 2.f) * (cfg.attack_level - cfg.sustain_level);
 	}
 	else {
-		output = cfg.sustain_level - powf((curr_time - off_time) / cfg.release_time, 2.f) * cfg.sustain_level;
+		output = cfg.sustain_level;
+	}
+	if (off_time >= 0.f) {
+		output *= powf(1.f - (curr_time - off_time) / cfg.release_time, 2.f);
 	}
 
 	output *= scale;
